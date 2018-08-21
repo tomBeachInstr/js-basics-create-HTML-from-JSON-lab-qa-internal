1bc332b4da6c965e41a3652c12774bacd72485b1
Thomas E Beach <thomas.beach@flatironschool.com>
2018-08-21 23:10:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
   console.log("== DOMContentLoaded ==");
+  console.log("movies:", movies);
 
   let titanicBtn = document.getElementById("Titanic");
   let terminatorBtn = document.getElementById("Terminator_2");
